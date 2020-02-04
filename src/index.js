866410d2d50349f54ac9e01ab716c6160ca7de69
TykhonchukIvan <tykhonchukivan@gmail.com>
2020-02-04 17:17:03 +0200

    export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-// <<<<<<< development_LinkedList
+const arrListES5 = require('./collections/arrayList').arrListES5;
+const arrListES6 = require('./collections/arrayListES6').arrListES6;
+const linkListES5 = require('./collections/linkedList').lListES5;
+const linkListES6 = require('./collections/linkedListES6').lListES6;
 
-// // const aa = require('./linkedList').tess;
-// //
-// =======
-// const v = require("./collections/ArrayList").test;
-
-// module.exports.test1 = v;
-
-// console.log(v);
-// >>>>>>> development_collections
